6d1f0c8ed2415043831f386a516e2972d6390ada
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2025-10-24 09:06:19 +0700

    adding mark-done and mark-in-progres functionality

--- main.go
+++ main.go
@@ -1,91 +1,105 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
 	"os"
-	"reflect"
-	"strconv"
 	"tast-tracker/task"
 )
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
 
 func main() {
 	args := os.Args
 	prefix := &args[1]
-	command := &args[2]
 	var secondArg *string
 	var thirdArg *string
 
-	file, err := os.ReadFile("./data/tasks.json")
-	if errors.Is(err, os.ErrNotExist) {
-		initTaskList, _ := json.Marshal(task.InitTaskList())
-		initTaskListJSON := []byte(initTaskList)
-		os.WriteFile("./data/tasks.json", initTaskListJSON, 0644)
-	}
+	var taskList task.TaskList = task.ReadTask()
 
-	var taskList task.TaskList = task.TaskList{}
-	json.Unmarshal(file, &taskList)
-	check(err)
-	
 	if *prefix == "task-cli" {
+		command := &args[2]
 		switch *command {
 		case "list":
-			for i, val := range taskList.Todo {
-				fmt.Printf("[%d] id: %s title: %s\n", i+1, val.Id, val.Title)
-			}
-			for i, val := range taskList.Done {
-				fmt.Printf("[%d] id: %s title: %s\n", i+1, val.Id, val.Title)
-			}
-			for i, val := range taskList.InProgres {
-				fmt.Printf("[%d] id: %s title: %s\n", i+1, val.Id, val.Title)
+			if len(args) >= 4 {
+				showOption := &args[3]
+				switch *showOption {
+				case "todo":
+					task.ShowTodoTask(&taskList)
+				case "done":
+					task.ShowDoneTask(&taskList)
+				case "in-progress":
+					task.ShowInProgresTask(&taskList)
+				default:
+					fmt.Println("Invalid argument")
+				}
+			} else {
+				task.ShowAllTask(&taskList)
 			}
 		case "add":
-			secondArg = &args[3]
-			if reflect.TypeOf(secondArg).String() == "*string" {
-				newId := fmt.Sprintf("%d", len(taskList.Todo)+1)
-				newTask := task.NewTask(newId, *secondArg)
+			if len(args) >= 4 {
+				secondArg = &args[3]
+				newTask := task.NewTask(*secondArg)
 				task.AddTask(&taskList.Todo, *newTask)
 				task.SaveTask(&taskList)
 				fmt.Println("Task successfully added.")
 			} else {
-				fmt.Println("Invalid type value.")
+				fmt.Println("Missing argument")
 			}
 		case "update":
 			secondArg = &args[3]
 			thirdArg = &args[4]
-			taskId, _ := strconv.ParseInt(*secondArg, 16, 64)
-			if reflect.TypeOf(taskId).String() == "int64" {
+			if len(args) >=5 {
 				task.UpdateTask(&taskList, *secondArg, *thirdArg)
 				task.SaveTask(&taskList)
 				fmt.Println("Task successfully updated.")
 			} else {
-				fmt.Println("Invalid type value.")
+				fmt.Println("Missing argument.")
 			}
 		case "delete":
-			secondArg = &args[3]
-			taskId, _ := strconv.ParseInt(*secondArg, 16, 64)
-			if reflect.TypeOf(taskId).String() == "int64" {
-				result := task.DeleteTask(&taskList.Todo, *secondArg)
+			if len(args) >= 4 {
+				secondArg = &args[3]
+				result := task.DeleteTask(&taskList, *secondArg)
 				if result {
 					task.SaveTask(&taskList)
+					fmt.Println("Task deleted")
 				} else {
 					fmt.Println("Task not found.")
 				}
+			} else {
+				fmt.Println("Missing argument. Expected Id.")
 			}
-		default :
+		case "mark-in-progres":
+			if len(args) >= 4 {
+				secondArg = &args[3]
+				result := task.InProgresTask(&taskList, *secondArg)
+
+				if result {
+					fmt.Println("Process done successfully.")
+				} else {
+					fmt.Println("Task not found.")
+				}
+			} else {
+				fmt.Println("Missing argument. Expected Id.")
+			}
+		case "mark-done":
+			if len(args) >= 4 {
+				secondArg = &args[3]
+				result := task.DoneTask(&taskList, *secondArg)
+				if result {
+					fmt.Println("Process done successfully.")
+				} else {
+					fmt.Println("Task not found.")
+				}
+			} else {
+				fmt.Println("Missing argument. Expected Id.")
+			}
+		default:
 			fmt.Println("Invalid Command")
 			fmt.Println("Available Command:")
 			fmt.Println("[*] show - Showing all task")
 			fmt.Println("[*] add - Add a new task")
 			fmt.Println("[*] update - Change title of a task")
 			fmt.Println("[*] delete - Delete a task")
+			fmt.Println("[*] mark-done - Mark a task to be done")
+			fmt.Println("[*] mark-in-progres - Mark a task to be in-progres")
 		}
 	} else {
 		fmt.Println("Invalid prefix. Use task-cli")
--- task/task.go
+++ task/task.go
@@ -1,8 +1,11 @@
 package task
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"math/rand"
 	"os"
 	"time"
 )
@@ -20,51 +23,177 @@ type Task struct {
 	UpdatedAt string
 }
 
+func GenerateId() string {
+	b := make([]byte, 4) //equals 8 characters
+	rand.Read(b)
+	s := hex.EncodeToString(b)
+	return s
+}
+
 func InitTaskList() TaskList {
 	var emptyTaskList = TaskList{Todo: []Task{}, Done: []Task{}, InProgres: []Task{}}
 	return emptyTaskList
 }
-func NewTask(id, title string) *Task {
-	return &Task{Id: id, Title: title, CreatedAt: time.Now().String(), UpdatedAt: time.Now().String()}
+func NewTask(title string) *Task {
+	return &Task{Id: GenerateId(), Title: title, CreatedAt: time.Now().String(), UpdatedAt: time.Now().String()}
 }
-func ShowAllTask(taskList *[]Task) {
+func ShowAllTask(taskList *TaskList) {
 	fmt.Println("=========\nList Task\n=========")
-	for i, v := range *taskList {
-		fmt.Printf("[%d] %s \n", i+1, v.Title)
+	ShowTodoTask(taskList)
+	ShowDoneTask(taskList)
+	ShowInProgresTask(taskList)
+}
+
+func ShowTodoTask(taskList *TaskList) {
+	fmt.Println("\n[*] Todo Task")
+	if len(taskList.Todo) > 0 {
+		for i, val := range taskList.Todo {
+			fmt.Printf("[%d] id: %s title: %s\n", i+1, val.Id, val.Title)
+		}
+	} else {
+		fmt.Println("List Empty")
 	}
 }
+func ShowDoneTask(taskList *TaskList) {
+	fmt.Println("\n[*] Done Task")
+	if len(taskList.Done) > 0 {
+		for i, val := range taskList.Done {
+			fmt.Printf("[%d] id: %s title: %s\n", i+1, val.Id, val.Title)
+		}
+	} else {
+		fmt.Println("List Empty")
+	}
+}
+func ShowInProgresTask(taskList *TaskList) {
+	fmt.Println("\n[*] In-Progres Task")
+	if len(taskList.InProgres) > 0 {
+		for i, val := range taskList.InProgres {
+			fmt.Printf("[%d] id: %s title: %s\n", i+1, val.Id, val.Title)
+		}
+	} else {
+		fmt.Println("List Empty")
+	}
+
+}
+
 func AddTask(taskList *[]Task, newTask Task) {
 	*taskList = append(*taskList, newTask)
 }
-func DeleteTask(taskList *[]Task, id string) bool {
-	newTaskList := []Task{}
-	for _, v := range *taskList {
+func DeleteTask(taskList *TaskList, id string) bool {
+	newTaskList := TaskList{}
+	for _, v := range taskList.Todo {
 		if v.Id != id {
-			newTaskList = append(newTaskList, v)
+			newTaskList.Todo = append(newTaskList.Todo, v)
+		}
+	}
+	for _, v := range taskList.Done {
+		if v.Id != id {
+			newTaskList.Done = append(newTaskList.Done, v)
+		}
+	}
+	for _, v := range taskList.InProgres {
+		if v.Id != id {
+			newTaskList.InProgres = append(newTaskList.InProgres, v)
 		}
 	}
 
-	if len(newTaskList) == len(*taskList) {
+	if len(newTaskList.Done) == len(taskList.Done) && len(newTaskList.InProgres) == len(taskList.InProgres) && len(newTaskList.Todo) == len(taskList.Todo) {
 		return false
 	}
 	*taskList = newTaskList
 	return true
 }
-func UpdateTask(taskList *[]Task, id, title string) bool {
-	for i, v := range *taskList {
+func UpdateTask(taskList *TaskList, id, title string) bool {
+	for i, v := range taskList.Todo {
 		if v.Id == id {
-			(*taskList)[i].Title = title
-			(*taskList)[i].UpdatedAt = time.Now().String()
+			(taskList.Todo)[i].Title = title
+			(taskList.Todo)[i].UpdatedAt = time.Now().String()
+			return true
+		}
+	}
+	for i, v := range taskList.Done {
+		if v.Id == id {
+			(taskList.Done)[i].Title = title
+			(taskList.Done)[i].UpdatedAt = time.Now().String()
+			return true
+		}
+	}
+	for i, v := range taskList.InProgres {
+		if v.Id == id {
+			(taskList.InProgres)[i].Title = title
+			(taskList.InProgres)[i].UpdatedAt = time.Now().String()
 			return true
 		}
 	}
 	return false
 }
-func SaveTask(taskList *[]Task) {
+func SaveTask(taskList *TaskList) {
 	allTask, err := json.Marshal(taskList)
 	allTaskJSON := []byte(allTask)
 	if err != nil {
 		panic(err)
 	}
 	os.WriteFile("./data/tasks.json", allTaskJSON, 0644)
+}
+
+func FindTaskInTodoAndInProgres(taskList *TaskList, id string) *Task {
+	for i, v := range taskList.Todo {
+		if v.Id == id {
+			return &taskList.Todo[i]
+		}
+	}
+	for i, v := range taskList.InProgres {
+		if v.Id == id {
+			return &taskList.InProgres[i]
+		}
+	}
+	return nil
+}
+
+func FindTaskInTodo(taskList *TaskList, id string) *Task {
+	for i, v := range taskList.Todo {
+		if v.Id == id {
+			return &taskList.Todo[i]
+		}
+	}
+	return nil
+}
+
+func DoneTask(taskList *TaskList, id string) bool {
+	foundTask := FindTaskInTodoAndInProgres(taskList, id)
+	if foundTask != nil {
+		DeleteTask(taskList, foundTask.Id)
+		taskList.Done = append(taskList.Done, *foundTask)
+		SaveTask(taskList)
+		return true
+	}
+	return false
+}
+
+func InProgresTask(taskList *TaskList, id string) bool {
+	foundTask := FindTaskInTodo(taskList, id)
+	if foundTask != nil {
+		DeleteTask(taskList, foundTask.Id)
+		taskList.InProgres = append(taskList.InProgres, *foundTask)
+		SaveTask(taskList)
+		return true
+	}
+	return false
+}
+
+func ReadTask() TaskList {
+	file, err := os.ReadFile("./data/tasks.json")
+	if errors.Is(err, os.ErrNotExist) {
+		initTaskList, _ := json.Marshal(InitTaskList())
+		initTaskListJSON := []byte(initTaskList)
+		os.WriteFile("./data/tasks.json", initTaskListJSON, 0644)
+		// attempt to read again if file not exist
+		file, _ := os.ReadFile("./data/tasks.json")
+		taskList := TaskList{}
+		json.Unmarshal(file, &taskList)
+		return taskList
+	}
+	taskList := TaskList{}
+	json.Unmarshal(file, &taskList)
+	return taskList
 }
